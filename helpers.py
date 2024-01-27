4e0525283cbb95b5591f6e53f254f8ce488a007f
Shaun <swsorour8@gmail.com>
2024-01-27 21:00:05 +0200

    initial commit

---
+++ helpers.py
@@ -0,0 +1,41 @@
+import random
+
+
+def convert_library_to_list(library):
+    word_list = open(library).read().split()
+
+    return word_list
+
+
+def replace_words(original_sentence, word_list):
+    original_sentence = original_sentence.split()
+    new_sentence = []
+
+    for word in original_sentence:
+        # reject special characters/numbers
+        if word.isalpha():
+            new_word = find_matching_word(word, word_list)
+            new_sentence.append(new_word)
+        else:
+            new_sentence.append(word)
+
+    modified_sentence = " ".join(new_sentence)
+
+    return modified_sentence
+
+
+def find_matching_word(original_word, word_list):
+    # filter based on criteria
+    # same first letter + length
+    matching_words = [
+        new_word
+        for new_word in word_list
+        if new_word[0] == original_word[0] and len(new_word) == len(original_word)
+    ]
+
+    if matching_words:
+        matching_word = random.choice(matching_words)
+    else:
+        matching_word = original_word
+
+    return matching_word
---
+++ main.py
@@ -0,0 +1,23 @@
+import helpers
+
+# Swap out library here
+library_file = "word-library.txt"
+word_list = helpers.convert_library_to_list(library_file)
+first_time = True 
+
+while True:
+    if first_time:
+        print('Welcome to Scrabble Pro! \U0001F3B2 \n'
+            '\nHow to: Type your sentence at the prompt, press Enter,'
+            ' watch the magic unfold \n')
+        first_time = False
+
+
+    input_sentence = input("Enter a sentence ('Esc' to quit): ")
+
+    if input_sentence.lower() == '\x1b':
+        print("Exiting program. Happy Scrabbling!")
+        break
+
+    new_sentence = helpers.replace_words(input_sentence, word_list)
+    print("\nUpdated sentence: " + new_sentence)
